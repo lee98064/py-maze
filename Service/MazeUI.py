bd196dabbc1ea58986cce23edb4a7e52296e1c6b
Ryan Lee <lee98064@gmail.com>
2022-05-26 20:45:14 +0800

    Add refresh and maze size dropdown element

--- Service/MazeUI.py
+++ Service/MazeUI.py
@@ -1,5 +1,7 @@
+import time
 from Service.Maze import Maze
-from PyQt5.QtWidgets import (QWidget, QPushButton, QComboBox)
+from PyQt5.QtWidgets import (
+    QWidget, QPushButton, QComboBox, QLabel, QGridLayout)
 
 
 class MazeUI(QWidget):
@@ -17,17 +19,34 @@ class MazeUI(QWidget):
         self.setGeometry(50, 50, self.width, self.height)
         self.setFixedSize(self.width, self.height)
 
+        # 建立mazeSizeComboBox Label
+        self.mazeSizeComboBoxLabel = QLabel(self)
+        self.mazeSizeComboBoxLabel.setText("請選擇地圖大小：")
+        self.mazeSizeComboBoxLabel.resize(200, 100)
+        self.mazeSizeComboBoxLabel.move(810, 100)
+
+        # 建立下拉式選單
+        self.mazeSizeComboBox = QComboBox(self)
+        self.mazeSizeComboBox.addItems(
+            ["{:02d}".format(x) for x in range(2, 21)])
+        self.mazeSizeComboBox.resize(200, 30)
+        self.mazeSizeComboBox.move(810, 10)
+        self.mazeSizeComboBox.setCurrentIndex(18)
+        self.mazeSizeComboBox.currentIndexChanged.connect(
+            self.change_maze_size)
+
         # 建立刷新按鈕
         self.refreshBtn = QPushButton("刷新", self)
-        self.refreshBtn.move(900, 10)
+        self.refreshBtn.resize(90, 30)
+        self.refreshBtn.move(1030, 10)
         self.refreshBtn.clicked.connect(self.create_maze)
 
         # 預設產生 20 * 20 地圖
         self.create_maze()
-        # self.MazeSize = QComboBox(self)
-        # self.MazeSize.addItems(["{:02d}".format(x) for x in range(1, 21)])
-        # self.MazeSize.resize(90, 50)
-        # self.MazeSize.move(450, 450)
+
+    def change_maze_size(self):
+        # print(self.mazeSizeComboBox.currentText())
+        self.mazeSize = int(self.mazeSizeComboBox.currentText())
 
     def create_maze(self):
         self.remove_maze()
---
+++ Service/SolveMaze.py
@@ -0,0 +1,4 @@
+class SolveMaze():
+
+    def __init__(self) -> None:
+        pass
